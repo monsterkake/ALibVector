9ac38351b89d83567baabbbae0fa0d5f3506dff1
monsterkake <56121889+monsterkake@users.noreply.github.com>
2021-07-13 17:59:08 +0300

    refactor

--- ALibVector/main.cpp
+++ ALibVector/main.cpp
@@ -6,59 +6,59 @@ using namespace ALib;
 
 int main()
 {
-    
-    
-    /*
-    for(int i = 0; i< 10;i++){
-       
-        vec.pushFront(i);
-    }
-    for (int i = 0; i < vec.size(); i++) {
-
-        std::cout << vec[i] << " " << std::endl;
-    }
-    for (int i = vec.size()-1; i >=0 ; i--) {
-
-        std::cout << vec[i] << " " << std::endl;
-    }
-    for (int i = vec.size() - 1; i >= 0; i--) {
-
-        vec.popBack();
-    }
-    for (int i = 0; i < 10; i++) {
-
-        vec.pushBack(i);
-    }
-    for (int i = vec.size() - 1; i >= 0; i--) {
-
-        std::cout << vec[i] << " " << std::endl;
-    }
-    
-    
-    while (true) 
-    {
-        
-        vec.pushBack(1);
-        vec.popBack();
-        std::cout << vec[0] << " " << std::endl;
-        //vec.pushFront(1);
-        //vec.popBack();
-    }
-    */
     Vector<int> vec;
-    for (int i = 0; i < 10; i++) {
+    //
+    //
+    //for(int i = 0; i< 10;i++){
+    //   
+    //    vec.pushFront(i);
+    //}
+    //for (int i = 0; i < vec.size(); i++) {
 
-        vec.pushFront(i);
-    }
-    vec[0] = 0;
-    vec[1] = 0;
-    vec[2] = 0;
-    vec[3] = 0;
+    //    std::cout << vec[i] << " " << std::endl;
+    //}
+    //for (int i = vec.size()-1; i >=0 ; i--) {
 
-    for (int i = vec.size() - 1; i >= 0; i--) {
+    //    std::cout << vec[i] << " " << std::endl;
+    //}
+    //for (int i = vec.size() - 1; i >= 0; i--) {
 
-        std::cout << vec[i] << " " << std::endl;
-    }
+    //    vec.popBack();
+    //}
+    //for (int i = 0; i < 10; i++) {
+
+    //    vec.pushBack(i);
+    //}
+    //for (int i = vec.size() - 1; i >= 0; i--) {
+
+    //    std::cout << vec[i] << " " << std::endl;
+    //}
+    //
+    //
+    ////while (true) 
+    ////{
+    ////    
+    ////    vec.pushBack(1);
+    ////    vec.popBack();
+    ////    std::cout << vec[0] << " " << std::endl;
+    ////    //vec.pushFront(1);
+    ////    //vec.popBack();
+    ////}
+    //
+    //
+    //for (int i = 0; i < 10; i++) {
+
+    //    vec.pushFront(i);
+    //}
+    //vec[0] = 0;
+    //vec[1] = 0;
+    //vec[2] = 0;
+    //vec[3] = 0;
+
+    //for (int i = vec.size() - 1; i >= 0; i--) {
+
+    //    std::cout << vec[i] << " " << std::endl;
+    //}
 
     system("pause");
 
--- ALibVector/vector.h
+++ ALibVector/vector.h
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <memory>
 
-namespace ALib 
+namespace ALib
 {
 	template <typename T>
 	class Node
@@ -14,122 +14,78 @@ namespace ALib
 		std::shared_ptr<T> value = std::shared_ptr<T>(new T);
 		std::shared_ptr<Node<T>> prevNode = nullptr;
 		std::shared_ptr<Node<T>> nextNode = nullptr;
-		~Node();
 	};
-
-	template <typename T>
-	Node<T>::~Node()
-	{
-
-	}
 
 	template <typename T>
 	class Vector
 	{
 	private:
 		unsigned int length = 0;
-		 std::shared_ptr<Node<T>> firstNode = nullptr;
-		 std::shared_ptr<Node<T>> lastNode = nullptr;
-		T& at(int index);
+		std::shared_ptr<Node<T>> firstNode = nullptr;
+		std::shared_ptr<Node<T>> lastNode = nullptr;
+		T& at(int index)
+		{
+			if (index < 0 || index > length)
+			{
+				std::cout << std::endl << "vector subscript is out of range!";
+				exit(0);
+			}
+
+			std::shared_ptr<Node<T>> temp = firstNode;
+			for (int i = 0; i < index; i++)
+			{
+				temp = temp->nextNode;
+			}
+			return *temp->value;
+		};
+
 	public:
-		unsigned int size();
-		void pushBack(T value);
-		void popBack();
-		void pushFront(T value);
-		T& operator [](int index);
+		unsigned int size()
+		{
+			return length;
+		};
+		void pushBack(T value)
+		{
+			if (lastNode != nullptr)
+			{
+				lastNode->nextNode = std::shared_ptr<Node<T>>(new Node<T>);
+				lastNode->nextNode->prevNode = lastNode;
+				lastNode = lastNode->nextNode;
 
+			}
+			else
+			{
+				lastNode = std::shared_ptr<Node<T>>(new Node<T>);
+				firstNode = lastNode;
+			}
+			length++;
+			*lastNode->value = value;
+		};
+		void popBack()
+		{
+			lastNode = lastNode->prevNode;
+			length--;
+		};
+		void pushFront(T value)
+		{
+			if (firstNode != nullptr)
+			{
+				firstNode->prevNode = std::shared_ptr<Node<T>>(new Node<T>);
+				firstNode->prevNode->nextNode = firstNode;
+				firstNode = firstNode->prevNode;
 
-		Vector();
-		~Vector();
+			}
+			else
+			{
+				firstNode = std::shared_ptr<Node<T>>(new Node<T>);
+				lastNode = firstNode;
+			}
+			*firstNode->value = value;
+			length++;
+		};
+		T& operator [](int index)
+		{
+			return at(index);
+		};
 	};
-
-	template <typename T>
-	inline Vector<T>::Vector()
-	{
-
-	}
-
-	template<typename T>
-	inline Vector<T>::~Vector()
-	{
-
-	}
-
-	template <typename T>
-	inline unsigned int Vector<T>::size()
-	{
-		return length;
-	}
-
-	template <typename T>
-	inline void Vector<T>::pushBack(T value)
-	{
-		if (lastNode != nullptr)
-		{
-			lastNode->nextNode = std::shared_ptr<Node<T>>(new Node<T>);
-			lastNode->nextNode->prevNode = lastNode;
-			lastNode = lastNode->nextNode;
-			
-		}
-		else
-		{
-			lastNode = std::shared_ptr<Node<T>>(new Node<T>);
-			firstNode = lastNode;
-		}
-		length++;
-		*lastNode->value = value;
-	}
-
-	template <typename T>
-	inline void Vector<T>::popBack()
-	{
-		lastNode = lastNode->prevNode;
-		//delete lastNode->nextNode;
-		//lastNode->nextNode->~shared_ptr();
-		//lastNode.
-		//lastNode->nextNode = nullptr;
-		length--;
-	}
-
-	template<typename T>
-	inline void Vector<T>::pushFront(T value)
-	{
-		if (firstNode != nullptr) 
-		{
-			firstNode->prevNode = std::shared_ptr<Node<T>>(new Node<T>);
-			firstNode->prevNode->nextNode = firstNode;
-			firstNode = firstNode->prevNode;
-			
-		}
-		else
-		{
-			firstNode = std::shared_ptr<Node<T>>(new Node<T>);
-			lastNode = firstNode;
-		}
-		*firstNode->value = value;
-		length++;
-	}
-
-	template <typename T>
-	inline T& Vector<T>::operator[](int index)
-	{
-		return at(index);
-	}
-
-	template <typename T>
-	inline T& Vector<T>::at(int index)
-	{
-		if (index < 0 || index > length)
-		{
-			std::cout << std::endl << "vector subscript is out of range!";
-			exit(0);
-		}
-
-		std::shared_ptr<Node<T>> temp = firstNode;
-		for (int i = 0; i < index; i++)
-		{
-			temp = temp->nextNode;
-		}
-		return *temp->value;
-	}
 }
